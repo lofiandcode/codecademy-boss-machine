437819c3172fabeab46d6fa17ce2417fcb53e605
lofiandcode <josephtrettevik@gmail.com>
2022-05-13 10:20:12 +0300

    Fix minionsRouter.param() and add GET /:minionId path

--- server/api.js
+++ server/api.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
+const morgan = require('morgan');
+
+apiRouter.use(morgan('tiny'));
 
 //Mount existing minionsRouter at the '/minions' path
 const minionsRouter = require('./minions');
--- server/minions.js
+++ server/minions.js
@@ -2,8 +2,36 @@ const express = require('express');
 const minionsRouter = express.Router();
 const db = require('./db');
 
+minionsRouter.param('minionId', (req, res, next, id) => {
+    try {
+        const foundMinion = db.getFromDatabaseById('minions', id);
+        if (foundMinion === undefined) {
+            next(new Error('The minion you are looking for could not be found.'));
+        } else {
+            console.log('In else')
+            req.minion = foundMinion;
+            next();
+        };
+    } catch (err) {
+        next(err);
+    }
+});
+
 minionsRouter.get('/', (req, res, next) => {
     res.send(db.getAllFromDatabase('minions'));
 })
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+})
+
+//Error Handler
+const errorHandler = (err, req, res, next) => {
+    const status = err.status || 500;
+    console.log(err.message);
+    res.status(status).send(err.message);
+}
+
+minionsRouter.use(errorHandler);
 
 module.exports = minionsRouter;
