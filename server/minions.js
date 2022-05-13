1154111ad2fdb5818f4b915fe5bee0750b7444da
lofiandcode <josephtrettevik@gmail.com>
2022-05-13 09:08:18 +0300

    Add GET for path /api/minions

---
+++ server/minions.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const minionsRouter = express.Router();
+const db = require('./db');
+
+minionsRouter.get('/', (req, res, next) => {
+    res.send(db.getAllFromDatabase('minions'));
+})
+
+module.exports = minionsRouter;
